e9e0902ee0621233303a77657eaa74c6dabdd831
Raj Chunara <Raj.Chunara@fdmgroup.com>
2019-10-08 11:51:59 -0400

    second change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig :{
-    apiKey: "AIzaSyAma1jEBK6ohuDHQP8QhlySCb00Im29HDY",
-    authDomain: "fir-crud-cd4d4.firebaseapp.com",
-    databaseURL: "https://fir-crud-cd4d4.firebaseio.com",
-    projectId: "fir-crud-cd4d4",
-    storageBucket: "fir-crud-cd4d4.appspot.com",
-    messagingSenderId: "962998595773",
-    appId: "1:962998595773:web:d0664d32073325a83687d4",
-    measurementId: "G-00V1CFH4WV"
+    //firebase api key
   }
 };
 
